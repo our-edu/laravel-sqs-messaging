d7e75eed41025ac6894cacae505e167c933afc7f
karim abdelaah <karim.abdullah@our-edu.net>
2025-12-18 15:41:20 +0200

    refactor: Update RabbitMqPublisherAdapter to allow nullable queueName and payload parameters

--- src/Drivers/RabbitMq/RabbitMqPublisherAdapter.php
+++ src/Drivers/RabbitMq/RabbitMqPublisherAdapter.php
@@ -9,10 +9,10 @@ use Support\RabbitMQ\ShouldPublish;
 
 /**
  * Adapter to publish RabbitMQ-compatible events to SQS
- * 
+ *
  * This allows you to use existing ShouldPublish events with SQS
  * without modifying the event classes.
- * 
+ *
  * Note: This requires the ShouldPublish interface from your RabbitMQ package.
  * If you don't have it, you can remove this file or create a compatible interface.
  */
@@ -20,16 +20,15 @@ class RabbitMqPublisherAdapter implements ShouldPublish
 {
     use Publishable;
 
-    private array $payload;
+    private array|null $payload;
+    private string|null $queueName;
 
-    public string $queue = 'payment_queue';
-    private string $queueName;
-
-    public function __construct(string $queueName, array $payload)
+    public function __construct(?string $queueName = null, ?array $payload = null)
     {
         $this->payload = $payload;
         $this->queueName = $queueName;
     }
+
     public function publishEventKey(): string
     {
         return $this->queueName;
